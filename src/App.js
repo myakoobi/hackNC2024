098f0b293494313b0ff014f543ac947f2576a6e2
Monkeyboyandpandamix <Maghamohammadi@guilford.edu>
2024-11-03 01:25:38 -0500

    i had to modify it to make it work

--- src/App.js
+++ src/App.js
@@ -21,11 +21,12 @@ const App = () => {
   const calculatePayments = () => {
     const loan = parseFloat(loanAmount);
     const rate = parseFloat(interestRate)/100/12 ; 
+    const months = 12;
     if (isNaN(loan)|| loan <= 0 || isNaN(rate) || rate <=0){
       setMonthlyPayment("Please enter valid numbers!");
       return; 
     }
-    const monthly = loan * rate / (1 - Math.pow(1 + rate, -12)); 
+    const monthly = loan * rate / (1 - Math.pow(1 + rate, -months)); 
     const result = `$${monthly.toFixed(2)} per month`;
     setMonthlyPayment(result);
     sendEmail(user.email, loanAmount, expenses, result);
@@ -41,6 +42,7 @@ const App = () => {
           <p className='body'> Enter some information so we can get it calculated! </p>
           <p className='body'> Enter your income: </p> <input type="number" id="income" name="income"/>
           <p className='body'> Enter the loan amount owed: </p> <input type="number" id="loan" name="loan" onChange={(e) => setLoanAmount(e.target.value)}/>
+          <p className='body'> Enter the interest rate: </p> <input type="number" id="interest" name="interest" onChange={(e) => setInterestRate(e.target.value)}/>
           <p className='body'> Enter your monthly expenses:</p> <input type="number" id="expenses" name="expenses" onChange={(e) => setExpenses(e.target.value)}/>
           <button onClick={calculatePayments}>Calculate Payment</button>
           <h2 className='result' id="paymentResult">{"Result: " + monthlyPayment}</h2> {/* Display the result here */}
